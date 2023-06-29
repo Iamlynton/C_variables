e8e2d700c79233a24cca82a62aee3298146dd61e
Makenalyn <lyntonmakena@gmail.com>
2023-06-29 10:10:00 +0300

    Code initializes variables of different types

---
+++ variable_initialization.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int integer;
+	char character;
+	float nowithdec;
+	double nowith2decimals;
+
+	// integer - a memory space holding the space of an int
+	// char - holds one byte of data ( 8 bits)
+	// double = 0.00
+	// float = 0.0000
+	
+	printf("Program defines initializes variable types");
+
+
+
+}
+
